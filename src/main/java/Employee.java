b8ab01f77ba11315fa13920a9848173f61ef2ff6
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-06-21 16:56:56 +0100

    Employee abstract class created

---
+++ src/main/java/Employee.java
@@ -0,0 +1,16 @@
+// abstract key word used as this is an abstract class (top of the class inheritance chain)
+public abstract class Employee {
+
+    private String name;
+    private String niNumber;
+    private Float salary;
+
+    public Employee(String name, String niNumber, Float salary) {
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    }
+
+    
+
+}
